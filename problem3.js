64e4d21a9d02224a43e80c377c30df12ee68ffcf
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-01-07 16:57:08 +0530

    Add problem 3

---
+++ problem3.js
@@ -0,0 +1,11 @@
+// Print all the even numbers greater than 1 and less than 25.
+
+for(let i = 2; i<=25; i+=2){
+    console.log(i);
+}
+
+for(let i = 1; i<=25; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
