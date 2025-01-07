b006a85fa930de91a7dc29eac753495bb22b6745
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-01-07 16:47:41 +0530

    Add problem 2

---
+++ problem2.js
@@ -0,0 +1,13 @@
+// Print all the number from 20 to 1 in decreasing order.
+
+let i = 20
+
+while (i >= 1 ) {
+    console.log(i);
+    i -= 1
+}
+
+for(let j = 20 ; j >= 1;){
+    console.log(j);
+    j -= 1
+}
