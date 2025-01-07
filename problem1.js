f36e1eb2a0ac80c32a6c5ef2639a2ea152230e05
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-01-07 16:30:17 +0530

    Add problem 1

---
+++ problem1.js
@@ -0,0 +1,16 @@
+// Write a programme to calculate sum of all the numbers from 1-10.
+
+let ans = 0
+let i = 1
+
+for( i = 1; i <= 10; i++){
+    ans += i
+}
+
+console.log(ans);
+
+while (i <= 10) {
+    ans += i;
+    i++
+}
+console.log(ans);
