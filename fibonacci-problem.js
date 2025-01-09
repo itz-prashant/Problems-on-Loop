c3941ed4755cfad2c03dd939d7db44af1bf6e8f9
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-01-09 17:18:01 +0530

    Add fibonacci problem

---
+++ fibonacci-problem.js
@@ -0,0 +1,21 @@
+// Given a value n , Write a code to proint the n fibonacci number .
+
+function fibonacci(n) {
+    if(n == 0){
+        console.log(0);
+        return
+    }if(n > 1){
+        console.log(0);
+        console.log(1)
+    }
+    let last = 1;
+    let secondLast = 0;
+    for (let i = 2; i <= n ; i++) {
+        let ans = last + secondLast;
+        console.log(ans);
+        secondLast = last;
+        last = ans;
+    }
+}
+
+fibonacci(7)
